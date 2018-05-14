de1bee6358196fff60ed7da810e47516290a6c08
ssbd021 <sbd021@126.com>
2018-05-14 18:35:24 +0800

    nothing

--- client/app/agora.config.js
+++
@@ -1,3 +0,0 @@
-export const APP_ID = ''; // appid obtained from https://dashboard.agora.io/signin/
-export const SIGNAL_ID = ''; // appid for signal, usually the same with the above one
-export const SERVER_URL = 'http://123.155.153.85:3232'; // server url, default to be our test url
